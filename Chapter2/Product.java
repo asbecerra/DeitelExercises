38a366e9b72d6347437e444c9d584b94a1464d07
Momo <momo@Momos-MacBook-Pro.local>
2020-01-21 19:41:42 -0500

    Autoevaluation chapter 2

---
+++ Chapter2/Product.java
@@ -0,0 +1,31 @@
+import java.uti1.Scanner;
+
+public class Product;       //calculate the product of three numbers
+
+{
+
+	public static void main (Strings[] args); {
+	
+		Scanner enter = new Scanner(System.in);
+
+		int x;
+		int y;
+		int z;
+		int product;
+
+		System.out.println("Enter de value for x");
+		x = enter.nextInt();
+
+		System.out.println("Enter de value for y");
+		y = enter.nextInt();
+
+		System.out.println("Enter de value for z");
+		z = enter.nextInt();
+
+		product = x*y*z;
+
+		System.out.println("The product is %d/n", product);
+
+	}
+}
+
