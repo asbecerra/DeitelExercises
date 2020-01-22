53041b7a97d787222be5a48d2a675068ac682f99
Momo <momo@Momos-MacBook-Pro.local>
2020-01-22 12:38:25 -0500

    Exercise 2.14. chapter 2

---
+++ Chapter2/Exercise_213.java
@@ -0,0 +1,23 @@
+public class Exercise_213 {
+
+	public static void main (String[] args) {
+
+		int a =1;
+		int b =2;
+		int c =3;
+		int d =4;
+
+
+		//Empleando println
+
+		System.out.println("1 2 3 4"); 
+
+		//Empleando print
+
+		System.out.print("1 2 3 4");
+
+		//Empleando printf
+
+		System.out.printf("\n%d %d %d %d\n ", a, b, c, d);
+	}	
+}
