551f55444cd6a0f04ff02d87dfef8090cb6ae074
AsBT <ansobete@gmail.com>
2020-01-27 23:36:29 -0500

    autoevaluation

--- Chapter3/Cuenta.java
+++ Chapter3/Cuenta.java
@@ -1,15 +1,16 @@
 public class Cuenta {
 	private double saldo; //variable guarda saldo
 	public Cuenta(double saldoInicial) {
-		if (saldoInicial > 0.0){
+		if (saldoInicial > 0.0) {
 			saldo = saldoInicial;
 		}
+	}
 
 	public void abonar(double monto) {
 		saldo = saldo + monto;
 	}
-	public double obtenerSaldo(){
+	public double obtenerSaldo() {
 		return saldo; 
 	}
-	}
+	
 }
