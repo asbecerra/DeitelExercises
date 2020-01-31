5c23b5a0cf5ee977a57d9adf1d39e320cafbac66
AsBT <ansobete@gmail.com>
2020-01-31 06:37:36 -0500

    corrections on EmployeeTest

--- Chapter3/EmployeeTest.java
+++ Chapter3/EmployeeTest.java
@@ -2,13 +2,17 @@ public class EmployeeTest {
 	public static void main(String[] args) {
 		Employee myEmployee1 = new Employee("Catalina", "MacOS", 78);
 		Employee myEmployee2 = new Employee("Sierra", "MacOS", -80);
-		System.out.printf("Employee1 Info\n name: %s\n Last name: %s\n salary: %.3f\n", myEmployee1.getFirstName(), myEmployee1.getLastName(), myEmployee1.getSalary());
-		System.out.printf("The anual Salary for Employee1 is:%.3f \n", myEmployee1.getAnualSalary());
+		EmployeeTest.printEmployeeInfo(myEmployee1);
+		EmployeeTest.printEmployeeInfo(myEmployee2);
 		myEmployee1.setSalary(myEmployee1.getSalary()*1.1);
-		System.out.printf("The Salary for Employee1 is goint to be: %.3f , if the salary raised up 10percent \n", myEmployee1.getSalary());
-		System.out.printf("Employee2 Info\n name: %s\n Last name: %s\n salary: %.3f\n", myEmployee2.getFirstName(), myEmployee2.getLastName(), myEmployee2.getSalary());
-		System.out.printf("The anual Salary for Employee2 is: ", myEmployee2.getAnualSalary());
 		myEmployee2.setSalary(myEmployee2.getSalary()*1.1);
-		System.out.printf("The Salary for Employee2 is goint to be: %.3f , if the salary raised up 10percent \n", myEmployee2.getSalary());
+		System.out.println("Raise 10%");
+		EmployeeTest.printEmployeeInfo(myEmployee1);
+		EmployeeTest.printEmployeeInfo(myEmployee2);
+	}
+	public static void printEmployeeInfo(Employee employee) {
+		System.out.printf("Employee Info\n name: %s\n Last name: %s\n salary: %.3f\n", employee.getFirstName(), employee.getLastName(), employee.getSalary());
+		System.out.printf("The anual Salary for Employee is:%.3f \n", employee.getAnualSalary());
+		System.out.println("-------------------------------------------");
 	}
 }
--- Chapter3/PruebaLibroCalificaciones.java
+++ Chapter3/PruebaLibroCalificaciones.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class PruebaLibroCalificaciones {
 	public static void main (String[] args){
 		//crea objeto Libro Calificaciones
-		LibroCalificaciones miLibroCalificaciones1 = new LibroCalificaciones("C5101 INTRODUCCIÓN A LA PROGRAMACIÓN CON JAVA", "Juan");
-		LibroCalificaciones miLibroCalificaciones2 = new LibroCalificaciones("C5102 ESTRUCTURA DE DATOS EN JAVA", "Pedro");
-		System.out.printf("El nombre del curso LibroCalificaciones es: %s\n, dirigido por: %s\n", miLibroCalificaciones1.obtenerNombreDelCurso(), miLibroCalificaciones1.obtenerNombreInstructor());		
-		System.out.printf("El nombre del curso LibroCalificaciones es: %s\n, dirigido por: %s\n", miLibroCalificaciones2.obtenerNombreDelCurso(), miLibroCalificaciones2.obtenerNombreInstructor());
+		LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones("C5101 INTRODUCCIÓN A LA PROGRAMACIÓN CON JAVA", "Juan");
+		//LibroCalificaciones miLibroCalificaciones2 = new LibroCalificaciones("C5102 ESTRUCTURA DE DATOS EN JAVA", "Pedro");
+		System.out.printf("El nombre del curso LibroCalificaciones es: %s\n", miLibroCalificaciones.obtenerNombreDelCurso());		
+		//System.out.printf("El nombre del curso LibroCalificaciones es: %s\n, dirigido por: %s\n", miLibroCalificaciones2.obtenerNombreDelCurso(), miLibroCalificaciones2.obtenerNombreInstructor());
+		miLibroCalificaciones.mostrarMensaje();
+		miLibroCalificaciones.determinarPromedioClase();
 	}
+
 }
